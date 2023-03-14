f74b0b3489da19f0d0bb5fdcc58e3c267a240954
Vania Petrova <VaniaPetrovaS@outlook.com>
2023-03-14 16:50:26 +0200

    Add project files.

---
+++ ConsoleApp23/Business/ProductBusiness.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp23.Common;
+using ConsoleApp23.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23.Business
+{
+    internal class ProductBusiness
+    {
+        private ProductData manager = new ProductData();
+        public List<Product> GetAll()
+        {
+            return manager.GetAll();
+        }
+        public void Add(Product product)
+        {
+            manager.Add(product);
+        }
+    }
+}
---
+++ ConsoleApp23/Data/ProductData.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp23.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23.Data
+{
+    internal class ProductData
+    {
+        public void Add(Product product)
+        {
+            using (var connection = Database.GetConnection())
+            {
+                var command = new SqlCommand("INSERT INTO product(Name, Price, Stock) VALUES(@name, @price, @stock)", connection);
+                command.Parameters.AddWithValue("name", product.Name);
+                command.Parameters.AddWithValue("price", product.Price);
+                command.Parameters.AddWithValue("stock", product.Stock);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+        public List<Product> GetAll()
+        {
+            var productList = new List<Product>();
+
+            using (var connection = Database.GetConnection())
+            {
+                var command = new SqlCommand("SELECT * FROM product", connection);
+                connection.Open();
+                using(var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var product = new Product(
+                            reader.GetInt32(0),
+                            reader.GetString(1),
+                            reader.GetDecimal(2),
+                            reader.GetInt32(3)
+                            );
+                        productList.Add(product);
+                    }
+                }
+                connection.Close();
+            }
+            return productList;
+        }
+    }
+}
